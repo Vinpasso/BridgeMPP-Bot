b29d94e752fc1d471857a7ea45d9cc00b2a36e08
Alexander Thole <alex@thole.org>
2014-12-03 15:31:59 +0100

    Dieing parrots now STFU faster

--- src/bots/ParrotBot/ParrotBot.java
+++ src/bots/ParrotBot/ParrotBot.java
@@ -211,11 +211,11 @@ public class ParrotBot{
 			statusqeue.add(ParrotBotMessages.stage2KillMessages[r.nextInt(ParrotBotMessages.stage2KillMessages.length)]);
 		}
 		statusqeue.add("Parrot " + colouredName + " is turning into a pool of blood.");
+		dead = true;
 		for(int i = 0; i < r.nextInt(101);i++){
 			statusqeue.add(ParrotBotMessages.stage3KillMessages[r.nextInt(ParrotBotMessages.stage3KillMessages.length)]);
 		}
 		statusqeue.add("Parrot " + colouredName + " is no more....");
-		dead = true;
 	}
 	
 	public boolean isDone(){
