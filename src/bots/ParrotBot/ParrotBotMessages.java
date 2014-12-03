1cc78603eba7e9cc307d259312c502874691b66b
Alexander Thole <alex@thole.org>
2014-12-03 15:23:53 +0100

    Added the additonal ParrotBotMessages

---
+++ src/bots/ParrotBot/ParrotBotMessages.java
@@ -0,0 +1,36 @@
+package bots.ParrotBot;
+
+public class ParrotBotMessages {
+
+	public static String[] stage1KillMessages = {
+		" is hardly holding on to his favourite place.",
+		" looks like he is curseing",
+		" looks at the hole in his body",
+		" is looking into the void",
+		"'s wound starts bleeding",
+		"'s life is zipping by infront of him"
+	};
+	public static String[] stage2KillMessages = {
+		" is breathing heavily.",
+		" tries to get up but fails.",
+		"'s feathers look more red",
+		" skipped a breath",
+		" sheds a tear, as the thought of his near deaht crossed his mind",
+		"'s vision starts to blur",
+		" tries to get away but he can hardly move anymore"
+	};
+	public static String[] stage3KillMessages = {
+		"'s eyes open rapidly then close slowly again.",
+		" looks as if he is sleeping",
+		" looks calm",
+		" skipped multipe breaths",
+		" looks as if he will haunt you",
+		"'s heart skipped a few beats",
+		" twitched and created a little wave in his pool of blood",
+		"'s pool of blood seems to grow larger",
+		" smiles as he though about all the happy moments in his life",
+		" smiles as he thinks about all the happy moments he had with you",
+		" looks sad and you belive he is thinking about his wife and his kids"
+	};
+	
+}
