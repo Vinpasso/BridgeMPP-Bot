5c62c6f7f7dbe910c2ac62382efc5e83f9328c30
Ediacarium <alex@thole.org>
2014-12-04 17:33:46 +0100

    Parrots now say their name while dieing

--- src/bots/ParrotBot/ParrotBot.java
+++ src/bots/ParrotBot/ParrotBot.java
@@ -204,16 +204,16 @@ public class ParrotBot{
 	public void kill(){
 		statusqeue.add("Parrot " + colouredName + " screams in pain.");
 		for(int i = 0; i < r.nextInt(ParrotBotMessages.stage1KillMessages.length+2);i++){
-			statusqeue.add(ParrotBotMessages.stage1KillMessages[r.nextInt(ParrotBotMessages.stage1KillMessages.length)]);
+			statusqeue.add("Parrot " + colouredName + ParrotBotMessages.stage1KillMessages[r.nextInt(ParrotBotMessages.stage1KillMessages.length)]);
 		}
 		statusqeue.add("Parrot " + colouredName + " fell from his favourite place.");
 		for(int i = 0; i < r.nextInt(ParrotBotMessages.stage2KillMessages.length+2);i++){
-			statusqeue.add(ParrotBotMessages.stage2KillMessages[r.nextInt(ParrotBotMessages.stage2KillMessages.length)]);
+			statusqeue.add("Parrot " + colouredName + ParrotBotMessages.stage2KillMessages[r.nextInt(ParrotBotMessages.stage2KillMessages.length)]);
 		}
 		statusqeue.add("Parrot " + colouredName + " is turning into a pool of blood.");
 		dead = true;
 		for(int i = 0; i < r.nextInt(ParrotBotMessages.stage3KillMessages.length+2);i++){
-			statusqeue.add(ParrotBotMessages.stage3KillMessages[r.nextInt(ParrotBotMessages.stage3KillMessages.length)]);
+			statusqeue.add("Parrot " + colouredName + ParrotBotMessages.stage3KillMessages[r.nextInt(ParrotBotMessages.stage3KillMessages.length)]);
 		}
 		statusqeue.add("Parrot " + colouredName + " is no more....");
 	}
