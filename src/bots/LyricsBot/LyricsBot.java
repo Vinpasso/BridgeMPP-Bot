a763747fe23c24584855458cc0b54a58ea5abcf6
Vinpasso <vinpasso@vinpasso.org>
2015-05-15 15:50:11 +0200

    So much SPAM... Too much SPAM

--- src/bots/LyricsBot/LyricsBot.java
+++ src/bots/LyricsBot/LyricsBot.java
@@ -20,7 +20,7 @@ public class LyricsBot extends Bot {
 	@Override
 	public void messageReceived(Message message) {
 		try {
-			if(message.getMessage().contains("?lyrics ") || (new Random()).nextInt(100) > 95)
+			if(message.getMessage().contains("?lyrics ") || (new Random()).nextInt(100) > 98)
 			{
 			sendMessage(new Message(message.getGroup(),
 					getSongData(message.getMessage().replaceAll("\\?lyrics ", "")), "Plain Text"));
