7bd6e536642faa27cbf0f7b71f25a08b400581b7
Vinpasso <vinpasso@vinpasso.org>
2015-05-27 15:56:29 +0200

    Fixing more Install Script

--- deploy/before_install.sh
+++ deploy/before_install.sh
@@ -5,7 +5,7 @@ cp deploy/id_rsa.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa.pub
 ssh-keyscan -H $SSH_HOST >> ~/.ssh/known_hosts
-export COMMIT_MESSAGE="$(git log -n 1 --stat | tr -d \'\"\$\\ )"
+export COMMIT_MESSAGE="$(git log -n 1 --stat | tr -d \'\"\$ )"
 echo $COMMIT_MESSAGE
 ssh $SSH_USER@$SSH_HOST "echo '0
 !usekey $BRIDGEMPP_KEY
@@ -14,5 +14,5 @@ ssh $SSH_USER@$SSH_HOST "echo '0
 A build has been triggered and is now commencing with build number: #$TRAVIS_BUILD_NUMBER
 This Repository on GitHub: https://github.com/Vinpasso/BridgeMPP-Bot
 This Repository on Travis: https://travis-ci.org/Vinpasso/BridgeMPP-Bot
-\'$COMMIT_MESSAGE\'
+$COMMIT_MESSAGE
 ' | nc -vw 5 127.0.0.1 $BRIDGEMPP_PORT"
