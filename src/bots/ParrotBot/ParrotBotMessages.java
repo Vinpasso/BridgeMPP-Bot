f2fb7f62be8d0a6b6a57d220952059fb084e54da
alex <ediacarium@github.com>
2015-06-30 09:33:48 +0200

    Changed a few death notes

--- src/bots/ParrotBot/ParrotBotMessages.java
+++ src/bots/ParrotBot/ParrotBotMessages.java
@@ -15,16 +15,16 @@ public class ParrotBotMessages {
 		" tries to get up but fails.",
 		"'s feathers look more red",
 		" skipped a breath",
-		" sheds a tear, as the thought of his near deaht crossed his mind",
+		" sheds a tear, as the thought of his near death crossed his mind",
 		"'s vision starts to blur",
 		" tries to get away but he can hardly move anymore"
 	};
 	public static String[] stage3KillMessages = {
-		"'s eyes open rapidly then close slowly again.",
+		"'s eyes open rapidly then close again slowly.",
 		" looks as if he is sleeping",
 		" looks calm",
 		" skipped multipe breaths",
-		" looks as if he will haunt you",
+		" looks as if he will haunt you eternally, once he is dead",
 		"'s heart skipped a few beats",
 		" twitched and created a little wave in his pool of blood",
 		"'s pool of blood seems to grow larger",
