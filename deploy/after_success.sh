7dc37f7931a39c77b02186cbb9c43618e63f5f04
Vinpasso <Vinpasso@users.noreply.github.com>
2015-07-09 08:41:11 +0200

    Update after_success.sh
    Fixed security issue of auto push

--- deploy/after_success.sh
+++ deploy/after_success.sh
@@ -1,3 +1,5 @@
+if [ $TRAVIS_PULL_REQUEST == "false" -a $TRAVIS_BRANCH == "master" ]
+then
 scp target/BridgeMPP-Bot-1.0.0-jar-with-dependencies.jar $SSH_USER@$SSH_HOST:$UPLOAD_PATH
 ssh $SSH_USER@$SSH_HOST "echo '$TRAVIS_BUILD_NUMBER' > $UPLOAD_PATH/version.txt"
 ssh $SSH_USER@$SSH_HOST "echo '0
@@ -19,3 +21,4 @@ New Version has been loaded
 New Version: $TRAVIS_BUILD_NUMBER
 ?botwrapper version
 ' | nc -vw 5 127.0.0.1 $BRIDGEMPP_PORT"
+fi
