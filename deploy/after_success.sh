546f67f0b49ab2bb482e1a612290f5c8b092e32e
Vinpasso <vinpasso@vinpasso.org>
2015-08-13 15:40:22 +0200

    Updated Build Scripts

--- deploy/after_success.sh
+++ deploy/after_success.sh
@@ -6,7 +6,7 @@ scp target/BridgeMPP-Bot-1.0.0-jar-with-dependencies.jar $SSH_USER@$SSH_HOST:$UP
 ssh $SSH_USER@$SSH_HOST "echo '$TRAVIS_BUILD_NUMBER' > $UPLOAD_PATH/version.txt"
 ssh $SSH_USER@$SSH_HOST "echo '0
 !usekey $BRIDGEMPP_KEY
-!createalias Build Bot
+!createalias Build\ Bot
 !subscribegroup $BRIDGEMPP_GROUP
 A build has succeeded.
 Upload Successful.
