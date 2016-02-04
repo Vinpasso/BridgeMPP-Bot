822fa1a18428bc53ec28a9196f69798987fa1bfa
Vinpasso <vinpasso@vinpasso.org>
2016-02-04 13:39:43 +0100

    Attempted improvement for Scheduling

--- src/main/java/bridgempp/bot/wrapper/Bot.java
+++ src/main/java/bridgempp/bot/wrapper/Bot.java
@@ -1,6 +1,7 @@
 package bridgempp.bot.wrapper;
 
 import io.netty.channel.Channel;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,8 +11,11 @@ import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Properties;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.logging.Level;
 
+import bridgempp.bot.messageformat.MessageFormat;
 import bridgempp.util.Log;
 
 /**
@@ -24,6 +28,8 @@ public abstract class Bot {
 	public String configFile;
 	public Properties properties;
 	protected Channel channel;
+	protected BlockingQueue<Message> incommingMessages = new ArrayBlockingQueue<Message>(50);
+	private volatile boolean processScheduled = false;
 
 	/**
 	 * Sets the Properties loaded from the Bot Configuration file
@@ -40,24 +46,27 @@ public abstract class Bot {
 	 */
 	public final void saveProperties() {
 		Log.log(Level.INFO, "Saving Properties for: " + name);
-		if(properties == null || properties.isEmpty())
-		{
+		if (properties == null || properties.isEmpty()) {
 			Log.log(Level.INFO, "Empty Bot Properties, aborted Save: " + name);
 			return;
 		}
 		try {
-			File tempFile = File.createTempFile("customParrot_" + configFile, ".parrot.tmp");
+			File tempFile = File.createTempFile("customParrot_" + configFile,
+					".parrot.tmp");
 			properties.store(
 					new FileOutputStream(tempFile),
 					"Bot Properties saved at: "
 							+ new SimpleDateFormat("DD.WW.yyyy HH:mm:ss")
 									.format(Date.from(Instant.now())));
-			if(tempFile.length() == 0)
-			{
-				throw new IOException("Write failed, temp file is length 0 after save");
+			if (tempFile.length() == 0) {
+				throw new IOException(
+						"Write failed, temp file is length 0 after save");
 			}
-			Files.move(tempFile.toPath(), new File(configFile).toPath(), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
-			Log.log(Level.INFO, "Saved Properties for: " + name + " to: " + configFile);
+			Files.move(tempFile.toPath(), new File(configFile).toPath(),
+					StandardCopyOption.REPLACE_EXISTING,
+					StandardCopyOption.ATOMIC_MOVE);
+			Log.log(Level.INFO, "Saved Properties for: " + name + " to: "
+					+ configFile);
 		} catch (IOException e) {
 			Log.log(Level.SEVERE,
 					"Failed to save Bot Config! Data Loss possible");
@@ -94,13 +103,48 @@ public abstract class Bot {
 	public void sendMessage(Message message) {
 		BotWrapper.printMessage(message, this);
 	}
-	
+
 	/**
-	 * Receive Messages in a synchronized Manner
+	 * Process Messages currently in the buffer
+	 * 
 	 * @param message
 	 */
-	public synchronized void synchronizedMessageReceived(Message message)
-	{
-		messageReceived(message);
+	public void processMessagesInQueue() {
+		while (!incommingMessages.isEmpty()) {
+			Message message = incommingMessages.poll();
+			if(message == null)
+			{
+				continue;
+			}
+			try
+			{
+				messageReceived(message);
+			} catch (Exception e)
+			{
+				BotWrapper.printMessage(new Message(message.getGroup(), "A Bot has crashed!\n" + e.toString() + "\n" + e.getStackTrace()[0].toString(), MessageFormat.PLAIN_TEXT), this);
+			}
+		}
+		processScheduled = false;
+	}
+
+	public synchronized void messageToQueue(Message message) {
+		try {
+			incommingMessages.put(message);
+		} catch (InterruptedException e) {
+			Log.log(Level.WARNING, "Incomming message interrupted", e);
+		}
+		if(!processScheduled)
+		{
+			scheduleExecuteProcess();
+			processScheduled = true;
+		}
+	}
+
+	private void scheduleExecuteProcess() {
+		Schedule.execute(new Runnable() {
+			public void run() {
+				processMessagesInQueue();
+			}
+		});
 	}
 }
--- src/main/java/bridgempp/bot/wrapper/network/IncommingMessageHandler.java
+++ src/main/java/bridgempp/bot/wrapper/network/IncommingMessageHandler.java
@@ -47,18 +47,7 @@ public class IncommingMessageHandler extends SimpleChannelInboundHandler<ProtoBu
 		{
 			bot.sendMessage(new Message(message.getGroup(), "This is Status Check triggered by " + bot.name + "\nResult:\n" + BotWrapper.statusCheck(), MessageFormat.PLAIN_TEXT));
 		}
-		Schedule.execute(new Runnable() {
-			public void run()
-			{
-				try
-				{
-					bot.synchronizedMessageReceived(message);
-				} catch (Exception e)
-				{
-					BotWrapper.printMessage(new Message(message.getGroup(), "A Bot has crashed!\n" + e.toString() + "\n" + e.getStackTrace()[0].toString(), MessageFormat.PLAIN_TEXT), bot);
-				}
-			}
-		});
+		bot.messageToQueue(message);
 	}
 
 	@Override
