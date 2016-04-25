463303a9e0d0aba771fa597ce2d96db850a83e0d
Vinpasso <Vinpasso@users.noreply.github.com>
2016-04-25 14:14:33 +0200

    Fixed optimization regex

--- src/main/java/bots/CustomParrotBot/LowerCaseMessageOptimization.java
+++ src/main/java/bots/CustomParrotBot/LowerCaseMessageOptimization.java
@@ -4,8 +4,8 @@ public class LowerCaseMessageOptimization
 {
 	public static void optimize(CustomParrot parrot)
 	{
-		parrot.condition = parrot.condition.replace("message.toLowerCase()", "lowerCaseMessage");
-		parrot.operation = parrot.operation.replaceAll("message.toLowerCase()", "lowerCaseMessage");
+		parrot.condition = parrot.condition.replaceAll("message\\.toLowerCase\\(\\)", "lowerCaseMessage");
+		parrot.operation = parrot.operation.replaceAll("message\\.toLowerCase\\(\\)", "lowerCaseMessage");
 	}
 
 }
