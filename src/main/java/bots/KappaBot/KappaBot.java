a8aa2c613c9ffb18c6b9d850067a431f5e8a8816
alex <ediacarium@github.com>
2016-05-07 17:22:23 +0200

    Fixed (debug) typo where http was changed to http

--- src/main/java/bots/KappaBot/KappaBot.java
+++ src/main/java/bots/KappaBot/KappaBot.java
@@ -43,7 +43,7 @@ public class KappaBot {
         try{
             //download the emote map. To use the full map use the following url: (may or may not work)
             // https://twitchemotes.com/api_cache/v2/global.json
-            fillMap(JsonReader.readJsonFromUrl("http://twitchemotes.com/api_cache/v2/global.json"));
+            fillMap(JsonReader.readJsonFromUrl("https://twitchemotes.com/api_cache/v2/global.json"));
             return null;
         }
         catch(Exception e){
