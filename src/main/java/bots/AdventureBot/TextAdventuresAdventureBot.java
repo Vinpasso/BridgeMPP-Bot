c4fc69123ce16efe56d285b64aff8a453794a89b
Vinpasso <vinpasso@vinpasso.org>
2016-08-02 11:19:37 +0200

    Dependency upgrade

--- src/main/java/bots/AdventureBot/TextAdventuresAdventureBot.java
+++ src/main/java/bots/AdventureBot/TextAdventuresAdventureBot.java
@@ -8,7 +8,8 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.javascript.host.KeyboardEvent;
+import com.gargoylesoftware.htmlunit.html.Keyboard;
+import com.gargoylesoftware.htmlunit.javascript.host.event.KeyboardEvent;
 
 import bots.AdventureBot.StringUtils.diff_match_patch;
 import bots.AdventureBot.StringUtils.diff_match_patch.Diff;
@@ -17,30 +18,32 @@ import bridgempp.bot.messageformat.MessageFormat;
 import bridgempp.bot.wrapper.Bot;
 import bridgempp.bot.wrapper.Message;
 
-public class TextAdventuresAdventureBot extends Bot {
+public class TextAdventuresAdventureBot extends Bot
+{
 
 	private WebClient webclient;
 	private HtmlPage htmlPage;
 	private diff_match_patch diff;
 	private String cached;
-	
+
 	@Override
-	public void initializeBot() {
-		
+	public void initializeBot()
+	{
+
 	}
 
 	@Override
-	public void messageReceived(Message message) {
-		if(message.getMessage().startsWith("?adventure "))
+	public void messageReceived(Message message)
+	{
+		if (message.getMessage().startsWith("?adventure "))
 		{
 			String command = message.getMessage().substring(11).trim();
-			if(command.contains(" "))
+			if (command.contains(" "))
 			{
 				command = command.substring(0, command.indexOf(" "));
 			}
 			interpretCommand(command, message);
-		}
-		else if(message.getMessage().startsWith("?ad") && htmlPage != null)
+		} else if (message.getMessage().startsWith("?ad") && htmlPage != null)
 		{
 			String command = message.getMessage().substring(4).trim();
 			enterQuery(command);
@@ -48,54 +51,58 @@ public class TextAdventuresAdventureBot extends Bot {
 		}
 	}
 
-	private void interpretCommand(String command, Message message) {
-		switch(command)
+	private void interpretCommand(String command, Message message)
+	{
+		switch (command)
 		{
-		case "play":
-			cached = "";
-			initializeGame(message.getMessage().substring(message.getMessage().lastIndexOf(" ") + 1));
-			sendMessage(new Message(message.getGroup(), "Welcome to TextAdventures", MessageFormat.PLAIN_TEXT));
-			sendMessage(new Message(message.getGroup(), getResponse(), MessageFormat.PLAIN_TEXT));
-			break;
-		case "stop":
-			webclient.closeAllWindows();
-			htmlPage = null;
-			webclient = null;
-			diff = null;
-			cached = null;
-			sendMessage(new Message(message.getGroup(), "A TextAdventure has been closed", MessageFormat.PLAIN_TEXT));
+			case "play":
+				cached = "";
+				initializeGame(message.getMessage().substring(message.getMessage().lastIndexOf(" ") + 1));
+				sendMessage(new Message(message.getGroup(), "Welcome to TextAdventures", MessageFormat.PLAIN_TEXT));
+				sendMessage(new Message(message.getGroup(), getResponse(), MessageFormat.PLAIN_TEXT));
+				break;
+			case "stop":
+				webclient.close();
+				htmlPage = null;
+				webclient = null;
+				diff = null;
+				cached = null;
+				sendMessage(new Message(message.getGroup(), "A TextAdventure has been closed", MessageFormat.PLAIN_TEXT));
 		}
-		
+
 	}
-	
+
 	private void initializeGame(String location)
 	{
-		if(webclient != null)
+		if (webclient != null)
 		{
-			webclient.closeAllWindows();
+			webclient.close();
 		}
 		diff = new diff_match_patch();
-		webclient = new WebClient(BrowserVersion.FIREFOX_24);
+		webclient = new WebClient(BrowserVersion.BEST_SUPPORTED);
 		webclient.getOptions().setThrowExceptionOnScriptError(false);
-		try {
+		try
+		{
 			htmlPage = webclient.getPage(location);
 			webclient.waitForBackgroundJavaScript(10000);
-		} catch (FailingHttpStatusCodeException | IOException e) {
+		} catch (FailingHttpStatusCodeException | IOException e)
+		{
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void enterQuery(String query)
 	{
-		try {
-			htmlPage.getFocusedElement().type(query + "\n");
-			htmlPage.getFocusedElement().type(KeyboardEvent.DOM_VK_RETURN);
-			webclient.waitForBackgroundJavaScript(10000);
-		} catch (IOException e) {
-			e.printStackTrace();
+		Keyboard keyboard = new Keyboard();
+		for (int i = 0; i < query.length(); i++)
+		{
+			keyboard.type(query.charAt(i));
 		}
+		keyboard.type('\n');
+		keyboard.press(KeyboardEvent.DOM_VK_RETURN);
+		webclient.waitForBackgroundJavaScript(10000);
 	}
-	
+
 	private String getResponse()
 	{
 		String text = htmlPage.asText();
@@ -104,25 +111,25 @@ public class TextAdventuresAdventureBot extends Bot {
 		diff.diff_charsToLines(differences, lines.lineArray);
 		String buffer = "";
 		Iterator<Diff> iterator = differences.iterator();
-		while(iterator.hasNext())
+		while (iterator.hasNext())
 		{
 			Diff diff = iterator.next();
-			switch(diff.operation)
+			switch (diff.operation)
 			{
-			case DELETE:
-				//buffer += "\n- " + diff.text;
-				break;
-			case EQUAL:
-				break;
-			case INSERT:
-				buffer += "\n+ " + diff.text;
-				break;
-			default:
-				break;
+				case DELETE:
+					// buffer += "\n- " + diff.text;
+					break;
+				case EQUAL:
+					break;
+				case INSERT:
+					buffer += "\n+ " + diff.text;
+					break;
+				default:
+					break;
 			}
 		}
 		cached = text;
 		return buffer.replaceAll("(\\s)\\s+", "$1").trim();
 	}
-	
+
 }
